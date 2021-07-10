ade03eafba2dc25bb0b93d732ba778b62066a25d
heyiamluke <luke.a@hotmail.co.uk>
2021-07-10 17:35:03 +0100

    try again

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ $config['db']['user'] = 'powevdyb';
 $config['db']['pass'] = 'lFv9sHOhTUPg';
 $config['db']['pre'] = 'tt_';
 $config['db']['port'] = '';
-$config['installed']='true';
+
 $config['version'] = '1.3';
-$config['lang']='english';
+
 ?>
