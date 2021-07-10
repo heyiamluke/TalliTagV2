4d3f20c4f30d635053f1ce29097e6a76833a85f5
heyiamluke <luke.a@hotmail.co.uk>
2021-07-10 17:27:33 +0100

    config fix

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickvcard';
+$config['db']['user'] = 'powevdyb';
+$config['db']['pass'] = 'lFv9sHOhTUPg';
+$config['db']['pre'] = 'tt_';
+$config['db']['port'] = '';
+$config['installed']='true;'
+$config['version'] = '1.3';
+?>
